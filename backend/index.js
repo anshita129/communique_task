653422e92b84890da3bbf5133c8334a1edc708e6
Anshita Batra <anshita@Anshitas-MacBook-Air.local>
2025-07-08 20:33:47 +0530

    again

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://communique-task-frontend.vercel.app"
+    "https://communique-task-frontend.vercel.app" 
+    
   ]
 }));
 
